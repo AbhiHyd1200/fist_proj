6072b22e4c09d575936824f9ee317d4809a09c94
AbhiHyd1200 <kottapalliabhinav@yahoo.com>
2019-12-08 10:00:57 +0530

    Message first

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("Hello, DevOps Class"); 
+    System.out.println("Hello,Weekend Fun"); 
   }
 }
 
